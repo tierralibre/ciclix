ce43438249641d54b631658995e5af0aa5db09e3
angel <angel.imaz@hush.com>
2018-08-07 20:23:32 +0200

    nerves

---
+++ ciclix_system/mix.exs
@@ -0,0 +1,96 @@
+defmodule CiclixSystem.MixProject do
+  use Mix.Project
+
+  @app :nerves_system_rpi0
+  @version Path.join(__DIR__, "VERSION")
+           |> File.read!()
+           |> String.trim()
+
+  def project do
+    [
+      app: :ciclix_system,
+      version: @version,
+      elixir: "~> 1.4",
+      compilers: Mix.compilers() ++ [:nerves_package],
+      nerves_package: nerves_package(),
+      description: description(),
+      package: package(),
+      deps: deps(),
+      aliases: [loadconfig: [&bootstrap/1], docs: ["docs", &copy_images/1]],
+      docs: [extras: ["README.md"], main: "readme"]
+    ]
+  end
+
+  def application do
+    []
+  end
+
+  defp bootstrap(args) do
+    System.put_env("MIX_TARGET", "rpi0")
+    Application.start(:nerves_bootstrap)
+    Mix.Task.run("loadconfig", args)
+  end
+
+  defp nerves_package do
+    [
+      type: :system,
+      # artifact_sites: [
+      #   {:github_releases, "nerves-project/#{@app}"}
+      # ],
+      platform: Nerves.System.BR,
+      platform_config: [
+        defconfig: "nerves_defconfig"
+      ],
+      checksum: package_files()
+    ]
+  end
+
+  defp deps do
+    [
+      {:nerves, "~> 1.0", runtime: false},
+      {:nerves_system_br, "1.4.0", runtime: false},
+      {:nerves_toolchain_armv6_rpi_linux_gnueabi, "1.0.0", runtime: false},
+      #{:nerves_toolchain_armv6_rpi_linux_gnueabi, "~> 1.1.0", nerves: [compile: true]},
+      {:nerves_system_linter, "~> 0.3.0", runtime: false},
+      {:ex_doc, "~> 0.18", only: :dev}
+    ]
+  end
+
+  defp description do
+    """
+    Nerves System - Raspberry Pi Zero and Zero W
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Timothy Mecklem", "Frank Hunleth"],
+      files: package_files(),
+      licenses: ["Apache 2.0"],
+      links: %{"Github" => "https://github.com/nerves-project/#{@app}"}
+    ]
+  end
+
+  defp package_files do
+    [
+      "LICENSE",
+      "mix.exs",
+      "nerves_defconfig",
+      "README.md",
+      "VERSION",
+      "rootfs_overlay",
+      "fwup.conf",
+      "fwup-revert.conf",
+      "post-createfs.sh",
+      "post-build.sh",
+      "cmdline.txt",
+      "linux-4.4.defconfig",
+      "config.txt"
+    ]
+  end
+
+  # Copy the images referenced by docs, since ex_doc doesn't do this.
+  defp copy_images(_) do
+    File.cp_r("assets", "doc/assets")
+  end
+end
---
+++ fw/config/config.exs
@@ -0,0 +1,80 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# Customize non-Elixir parts of the firmware.  See
+# https://hexdocs.pm/nerves/advanced-configuration.html for details.
+config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
+
+# Use shoehorn to start the main application. See the shoehorn
+# docs for separating out critical OTP applications such as those
+# involved with firmware updates.
+config :shoehorn,
+  init: [:nerves_runtime, :nerves_init_gadget],
+  app: Mix.Project.config()[:app]
+
+# Import target specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+# Uncomment to use target specific configurations
+
+# import_config "#{Mix.Project.config[:target]}.exs"
+
+
+config :nerves_firmware_ssh,
+  authorized_keys: [
+    File.read!(Path.join(System.user_home!(), ".ssh/id_rsa.pub"))
+  ]
+
+config :nerves_init_gadget,
+  ifname: "wlan0",
+  address_method: :dhcp,
+  mdns_domain: "ciclix.local",
+  node_name: nil,
+  node_host: :mdns_domain
+
+config :nerves_network, regulatory_domain: "ES"
+
+key_mgmt = System.get_env("NERVES_NETWORK_KEY_MGMT") || "WPA-PSK"
+
+config :nerves_network, :default,
+  wlan0: [
+    ssid: System.get_env("NERVES_NETWORK_SSID"),
+    psk: System.get_env("NERVES_NETWORK_PSK"),
+    key_mgmt: String.to_atom(key_mgmt)
+  ],
+  eth0: [
+    ipv4_address_method: :dhcp
+  ]
+
+config :ui, UiWeb.Endpoint,
+  http: [port: 80],
+  url: [host: "origin.local", port: 80],
+  # url: [host: "192.168.0.102", port: 80],
+  secret_key_base: "6ccqlxld2X6JaYZzAXdvjLfsrI9d22Wk6mRJjlm2M39kojzcvZqmSzbVMzHIBVZJ",
+  root: Path.dirname(__DIR__),
+  server: true,
+  render_errors: [accepts: ~w(html json)],
+  pubsub: [name: Ui.PubSub, adapter: Phoenix.PubSub.PG2]
+
+# config :ui, Ui.Repo,
+#   adapter: Sqlite.Ecto2,
+#   database: "/root/#{Mix.env()}.sqlite3"
+#   pool: 5
+
+# config :ui, ecto_repos: [Ui.Repo]
+
+# import_config "#{Mix.Project.config[:target]}.exs"
+
+# ntpd binary to use
+config :nerves_ntp, :ntpd, "/usr/sbin/ntpd"
+
+# servers to sync time from
+config :nerves_ntp, :servers, [
+    "0.pool.ntp.org",
+    "1.pool.ntp.org",
+    "2.pool.ntp.org",
+    "3.pool.ntp.org"
+  ]
